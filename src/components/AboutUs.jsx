c188c53420e93af8a375f6118d001f8316c95b94
vgalante2 <vgalante0@gmail.com>
2023-10-20 18:29:39 -0400

    final

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import SubmissionArea from "./SubmissionArea"
+
+
+function AboutUs() {
+  return (
+    <section className="about-section">
+    <div className="about-container">
+
+    </div>
+    </section>
+  );
+}
+
+export default AboutUs;
